3d7cc7caca32d82e0fcc470459a459102aa177d8
omars <omarassir12@gmail.com>
2026-07-01 15:18:24 +0300

    fix: add typescript module declarations for react-multi-date-picker locales/calendars

---
+++ nextjs/types/react-multi-date-picker.d.ts
@@ -0,0 +1,9 @@
+declare module 'react-multi-date-picker/locales/arabic' {
+  const arabic: any;
+  export default arabic;
+}
+
+declare module 'react-multi-date-picker/calendars/arabic_umalqura' {
+  const arabic_umalqura: any;
+  export default arabic_umalqura;
+}
